f712a215849a4b8be1e31b22f5d58171abb6041d
Quadtree <quadtree@gmail.com>
2018-08-10 21:36:24 -0700

    Updated

--- core/src/info/quadtree/ld42/GameState.java
+++ core/src/info/quadtree/ld42/GameState.java
@@ -12,6 +12,8 @@ public class GameState {
     public static final int GRID_WIDTH = 22;
     public static final int GRID_HEIGHT = 44;
 
+    public static final int CENTER_VARIANCE = 8;
+
     public GameState(){
 
 
@@ -21,20 +23,21 @@ public class GameState {
     public void generate(){
         while(true) {
             hexes = new Hex[GRID_WIDTH * GRID_HEIGHT];
-            setHex(new Hex(MathUtils.random(GRID_WIDTH / 2 - 8, GRID_WIDTH / 2 + 8), MathUtils.random(GRID_HEIGHT / 2 - 8, GRID_HEIGHT / 2 + 8), 1));
-            setHex(new Hex(MathUtils.random(GRID_WIDTH / 2 - 8, GRID_WIDTH / 2 + 8), MathUtils.random(GRID_HEIGHT / 2 - 8, GRID_HEIGHT / 2 + 8), 1));
-            setHex(new Hex(MathUtils.random(GRID_WIDTH / 2 - 8, GRID_WIDTH / 2 + 8), MathUtils.random(GRID_HEIGHT / 2 - 8, GRID_HEIGHT / 2 + 8), 1));
+            setHex(new Hex(MathUtils.random(GRID_WIDTH / 2 - CENTER_VARIANCE, GRID_WIDTH / 2 + CENTER_VARIANCE*2), MathUtils.random(GRID_HEIGHT / 2 - CENTER_VARIANCE, GRID_HEIGHT / 2 + CENTER_VARIANCE*2), 1));
+            setHex(new Hex(MathUtils.random(GRID_WIDTH / 2 - CENTER_VARIANCE, GRID_WIDTH / 2 + CENTER_VARIANCE*2), MathUtils.random(GRID_HEIGHT / 2 - CENTER_VARIANCE, GRID_HEIGHT / 2 + CENTER_VARIANCE*2), 1));
+            setHex(new Hex(MathUtils.random(GRID_WIDTH / 2 - CENTER_VARIANCE, GRID_WIDTH / 2 + CENTER_VARIANCE*2), MathUtils.random(GRID_HEIGHT / 2 - CENTER_VARIANCE, GRID_HEIGHT / 2 + CENTER_VARIANCE*2), 1));
 
-            for (int i = 0; i < 30; ++i) {
+            for (int i = 0; i < 40; ++i) {
                 long hexCount = Arrays.stream(hexes).filter(Objects::nonNull).count();
 
                 final int fi = i;
                 Arrays.stream(hexes).filter(Objects::nonNull).forEach(it -> {
-                    if (MathUtils.random(3) != 0) it.ttl++;
                     if (MathUtils.random(2) == 0 || hexCount < 200) {
-                        HexPos neighbor = it.getNeighbors()[MathUtils.random(5)];
+                        HexPos neighbor = MathUtils.randomBoolean() ? it.getNeighbors()[MathUtils.random(1) * 3 + 2] : it.getNeighbors()[MathUtils.random(5)];
                         if (!(neighbor instanceof Hex)) {
                             setHex(new Hex(neighbor.x, neighbor.y, 1));
+                        } else {
+                            ((Hex) neighbor).ttl++;
                         }
                     }
                 });
